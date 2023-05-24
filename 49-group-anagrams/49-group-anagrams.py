54e2d91951a5cfb53e6544d75a696a4151f8ac1b
andtomorrow <68937273+andtomorrow@users.noreply.github.com>
2023-05-24 13:34:07 +0900

    Time: 114 ms (51.18%), Space: 19.7 MB (53.73%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,13 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
 
-        combinations_dct = collections.defaultdict(list)
-        result_lst = []
+        anagrams = collections.defaultdict(list)
+        for word in strs:
+            anagrams["".join(sorted(word))].append(word)
 
-        for i in range(len(strs)):
-            combinations_dct["".join(sorted(strs[i]))].append(strs[i])
-
-        for k in combinations_dct.keys():
-            result_lst.append(combinations_dct[k])
-
-        return result_lst
+        return [*anagrams.values()]
