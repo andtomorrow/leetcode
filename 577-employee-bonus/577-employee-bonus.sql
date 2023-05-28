38a85b120da9c848850f2db94f50eafd021a837e
andtomorrow <68937273+andtomorrow@users.noreply.github.com>
2023-05-28 12:08:18 +0900

    Time: 2056 ms (28.54%), Space: 0B (100.00%) - LeetHub

--- 577-employee-bonus/577-employee-bonus.sql
+++ 577-employee-bonus/577-employee-bonus.sql
@@ -1,4 +1,8 @@
-# Write your MySQL query statement below
-SELECT L.name, R.bonus AS bonus
-FROM Employee L LEFT JOIN Bonus R ON L.empId=R.empId
-WHERE R.bonus < 1000 OR R.bonus IS NULL;
+# # Write your MySQL query statement below
+# SELECT L.name, R.bonus AS bonus
+# FROM Employee L LEFT JOIN Bonus R ON L.empId=R.empId
+# WHERE R.bonus < 1000 OR R.bonus IS NULL;
+
+SELECT L.name, R.bonus
+FROM Employee L LEFT JOIN Bonus R USING (empId)
+WHERE COALESCE(R.bonus, 0) < 1000;
